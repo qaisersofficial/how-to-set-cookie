ac364f805d91080ced25791f405d71151725e8e5
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 17:35:02 +0500

    Remove unused plaintext password variable from app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const bcrypt = require('bcrypt');
-const myPlaintextPassword = 'password';
-const someOtherPlaintextPassword = 'not_password';
+const myPlaintextPassword = 'password'; // = $2b$10$T.gk8Ygrw9eQnvSP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa
 const saltRounds = 10;
 app.get('/', (req, res) => {
     bcrypt.genSalt(saltRounds, (err, salt) => {
