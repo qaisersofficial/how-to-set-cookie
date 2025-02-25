3c9fd43baa2dd94ae74a53103b4cfd69a85e5145
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 20:12:37 +0500

    Add cookie parsing and JWT handling in root route

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
    let token = jwt.sign({ email: "qaisers.off@exm.com" }, 'secret');
-   console.log(token);
+   res.cookie('token', token);
+   res.send('Cookie is set');
 });
+
+app.get('/get', (req, res) => {
+    let token = req.cookies.token;
+    let decoded = jwt.verify(token, 'secret');
+    res.send(decoded);
+    // console.log(decoded);
+}
+);
 
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
