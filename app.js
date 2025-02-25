c82336ddae475d5f866ff4efdc6e77524c884185
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 17:35:56 +0500

    Remove bcrypt password hashing logic from root route and simplify response

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ const port = 3000;
 const app = express();
 const bcrypt = require('bcrypt');
 const myPlaintextPassword = 'password'; // = $2b$10$T.gk8Ygrw9eQnvSP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa
-const saltRounds = 10;
 app.get('/', (req, res) => {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
-            console.log(hash);
-        });
-    });
+    res.send('Working');
 });
 
     app.listen(port, () => {
