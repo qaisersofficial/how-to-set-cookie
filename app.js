ab85a31e74bb5f7e39adf0bb59e236e5dcee9940
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 17:39:28 +0500

    when i change my real hash with anyother hash the ouput will be Password is incorrect

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const bcrypt = require('bcrypt');
 const myPlaintextPassword = 'password'; // = $2b$10$T.gk8Ygrw9eQnvSP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa
 app.get('/', (req, res) => {
-    bcrypt.compare(myPlaintextPassword, '$2b$10$T.gk8Ygrw9eQnvSP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa', function(err, result) {
+    bcrypt.compare(myPlaintextPassword, '$2b$10$T.SP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa', function(err, result) {
         if (result) {
             res.send('Password is correct');
         } else {
