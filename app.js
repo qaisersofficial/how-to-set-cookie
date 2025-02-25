335d54012170cf94724b20ef0614ac6d8218d3ed
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 17:34:29 +0500

    Implement bcrypt password hashing in the root route and done encryption

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
-// const cookieParser = require('cookie-parser');
 const port = 3000;
 const app = express();
-
-// app.use(cookieParser());
-
+const bcrypt = require('bcrypt');
+const myPlaintextPassword = 'password';
+const someOtherPlaintextPassword = 'not_password';
+const saltRounds = 10;
 app.get('/', (req, res) => {
-//    res.cookie("name", "Qaiser Ali")
-   res.send('Cookie is set');
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
+            console.log(hash);
+        });
     });
+});
 
-    // app.get('/read', (req, res) => {
-    //     console.log(req.cookies);
-    //     res.send('Reading cookies from browser');
-    //      });
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
