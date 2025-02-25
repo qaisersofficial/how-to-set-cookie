5724764def43dbdf7a7a3c0d9a006c8cc5212194
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 17:38:30 +0500

    Implement password verification in root route using bcrypt and the output will be Password is correct

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 const bcrypt = require('bcrypt');
 const myPlaintextPassword = 'password'; // = $2b$10$T.gk8Ygrw9eQnvSP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa
 app.get('/', (req, res) => {
-    res.send('Working');
+    bcrypt.compare(myPlaintextPassword, '$2b$10$T.gk8Ygrw9eQnvSP/oYvMOLmJ3mT78XPg0W9x81DEtOsnxeVW9tOa', function(err, result) {
+        if (result) {
+            res.send('Password is correct');
+        } else {
+            res.send('Password is incorrect');
+        }
+    });
 });
 
     app.listen(port, () => {
