4dbc271929eca72ab7ab8e1c923d4b60fcafb6e7
QAISER ALI <qaisers.official@gmail.com>
2025-02-25 17:22:07 +0500

    Add cookie parsing and enhance cookie handling in app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const port = 3000;
 const app = express();
+
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-   res.cookie("name", "Qaiser")
+   res.cookie("name", "Qaiser Ali")
    res.send('Cookie is set');
     });
 
+    app.get('/read', (req, res) => {
+        console.log(req.cookies);
+        res.send('Reading cookies from browser');
+         });
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
